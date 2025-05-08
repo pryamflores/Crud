1611b7c02883b21b8d0d80e08e91184ce8f1f6f2
pryamflores <pryamflores0@gmail.com>
2025-05-08 10:36:15 +0800

    adding click button and tailwind design

---
+++ app/monitoring/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function monitoring() {
+  return (
+    <div>
+      <div>mornitoring</div>
+      <button className="pt-6">click me</button>
+    </div>
+  )
+}

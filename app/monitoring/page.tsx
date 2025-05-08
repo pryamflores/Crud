bcc9d3014e9e67ebf2eff11e94b8b3f232058935
pryamflores <pryamflores0@gmail.com>
2025-05-08 10:47:02 +0800

    update design buttons

--- app/monitoring/page.tsx
+++ app/monitoring/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export default function monitoring() {
-  return (
-    <div>
-      <div>mornitoring</div>
-      <button className="pt-6">click me</button>
-    </div>
-  )
+export default function Monitoring() {
+    return (
+        <div className="monitoring-container">
+            <h1 className="text-xl font-bold">Monitoring</h1>
+            <div className="button-group">
+                <button className="pt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+                    Click Me
+                </button>
+                <button className="pt-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none">
+                    Button 2
+                </button>
+                <button className="pt-6 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none">
+                    Button 3
+                </button>
+            </div>
+        </div>
+    );
 }
